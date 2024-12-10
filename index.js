fd8fb178a8f27260c76fd50c5a3370041d44dcf1
Chetan <chetanchauhan1394@gmail.com>
2024-12-10 18:32:32 +0530

    schools CRUD operation

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.delete(`/api/students/:id`, async (req, res) => {
   }
 });
 
+// update student's details
 app.patch("/api/students/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -73,6 +74,77 @@ app.patch("/api/students/:id", async (req, res) => {
       const response = await client.query(query);
       if (response.rowCount == 1) {
         return res.status(200).send(`Student's record updated...`);
+      } else {
+        return res.status(404).send("Record not found...");
+      }
+    } else {
+      return res.status(400).send("No fields provided for update");
+    }
+  } catch (error) {
+    return res.status(500).send("Internal Server Error...");
+  }
+});
+
+
+// get all schools
+app.get('/api/schools', async (req, res) => {
+  const query = `SELECT * FROM schools`;
+  const response = await client.query(query);
+  if(response.rowCount > 0) {
+    return res.json(response.rows)
+  } else {
+    return res.send('Record not found')
+  }
+})
+
+// delete school
+app.delete('/api/schools/:id', async (req, res) => {
+  const { id } = req.params;
+  const response = await client.query(`DELETE FROM schools WHERE id = '${id}'`)
+  if(response.rowCount == 0) return res.status(404).send('Record not found...')
+  res.status(200).send('Record deleted successfully');
+})
+
+// create new school
+app.post('/api/schools/createSchool', async (req, res)=> {
+  const { schoolName, registrationNo } = req.body;
+  const schoolId = uuidv4();
+  try {
+    const query = `INSERT INTO schools (
+      id, 
+      schoolName,
+      registrationNo
+    ) VALUES (
+      '${schoolId}',
+      '${schoolName}',
+      ${registrationNo}
+    )`;
+    const response = await client.query(query);
+    res.status(200).send('New school added successfully...');    
+  } catch (error) {
+    console.log('Error while creating school...', error);
+    res.status(500).send('Internal server error...')
+  }
+})
+
+// update school
+app.patch('/api/schools/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const requestBody = req.body;
+    if (requestBody && Object.keys(requestBody).length > 0) {
+      let clause = Object.keys(requestBody)
+        .map((key, index) => {
+          return !["schoolName"].includes(key)
+            ? `${key} = ${requestBody[key]}`
+            : `${key} = '${requestBody[key]}'`;
+        })
+        .join(", ");
+
+      const query = `UPDATE schools SET ${clause} WHERE id = '${id}'`;
+      const response = await client.query(query);
+      if (response.rowCount == 1) {
+        return res.status(200).send(`School's record updated...`);
       } else {
         return res.status(404).send("Record not found...");
       }
